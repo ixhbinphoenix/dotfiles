507c687906f16f011b515d95628851b5a3177a8d
ixhbinphoenix <47122082+ixhbinphoenix@users.noreply.github.com>
2022-01-13 16:50:55 +0100

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -6,31 +6,67 @@ echo "SCRIPT NOT TESTED!"
 echo "Script will continue in 1 seconds..."
 sleep 1
 
-
 cd $HOME
 
 echo "Backing up old dotfiles..."
+
+# Zsh
+
 mv .zshrc .zshrc.predot 2>/dev/null
-# not quite sure how the actual installed and not cloned dir is named
 mv .oh-my-zsh .oh-my-zsh.predot 2>/dev/null
 mv .ohmyzsh .ohmyzsh.predot 2>/dev/null
 mv .zsh_custom .zsh_custom.predot 2>/dev/null
+
+# Other ~ files
+
 mv .alacritty.yml .alacritty.yml.predot 2>/dev/null
 mv .actrc .actrc.predot 2>/dev/null
+
+#
+# .config folder files
+#
+
+# rice
 mv .config/kitty .config/kitty.predot 2>/dev/null
 mv .config/i3 .config/i3.predot 2>/dev/null
+mv .config/dunst .config/dunst.predot 2>/dev/null
 mv .config/rofi .config/rofi.predot 2>/dev/null
 mv .config/picom.conf .config/picom.conf.predot 2>/dev/null
+
+# other .config
+mv .config/lvim .config/lvim.predot 2>/dev/null
+
+#
+# /etc/
+#
+
+doas mv /etc/pacman.conf /etc/pacman.conf.predot 2>/dev/null
 echo "Completed!"
 
 echo "Symlinking new dotfiles..."
+
+# Zsh
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/.zsh_custom ~/.zsh_custom
+
+# Other ~ files
 ln -s ~/dotfiles/.alacritty.yml ~/.alacritty.yml
 ln -s ~/dotfiles/.actrc ~/.actrc
+
+# .config
+
+# rice
 ln -s ~/dotfiles/.config/kitty ~/.config/kitty
 ln -s ~/dotfiles/.config/rofi ~/.config/rofi
 ln -s ~/dotfiles/.config/i3 ~/.config/i3
+ln -s ~/dotfiles/.config/dunst ~/.config/dunst
 ln -s ~/dotfiles/.config/picom.conf ~/.config/picom.conf
+
+# other .config
+ln -s ~/dotfiles/.config/lvim ~/.config/lvim
+
+# /etc/
+
+doas ln -s ~/dotfiles/etc/pacman.conf /etc/pacman.conf
 echo "Completed!"
