fc296c347bd96dfb8bc6337d1626c2c3afcb5958
ixhbinphoenix <47122082+ixhbinphoenix@users.noreply.github.com>
2022-05-12 21:40:31 +0200

    Added feline and gitsigns

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,8 @@
 local home = os.getenv("HOME")
 
+vim.opt.tgc = true
 vim.opt.hidden = true
 vim.opt.encoding = "utf-8"
 
 require('plugins')
+require('feline').setup()
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,7 +2,15 @@
 
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
+	use {
+		'lewis6991/gitsigns.nvim',
+		config = function()
+			require('gitsigns').setup()
+		end
+	}
 	use 'sheerun/vim-polyglot'
 	use { 'neoclide/coc.nvim', branch = 'release' }
+	use 'kyazdani42/nvim-web-devicons'
+	use 'feline-nvim/feline.nvim'
 	use 'andweeb/presence.nvim'
 end)
