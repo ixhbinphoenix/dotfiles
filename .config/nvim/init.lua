67575184ba42249ea544ab4e1f7698cb220b7bde
ixhbinphoenix <47122082+ixhbinphoenix@users.noreply.github.com>
2022-05-14 15:23:41 +0200

    Enabled NvimTree autoresize

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -92,7 +92,7 @@ require('nvim-tree').setup({
     },
     open_file = {
       quit_on_open = false,
-      resize_window = false,
+      resize_window = true,
       window_picker = {
         enable = true,
         chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
