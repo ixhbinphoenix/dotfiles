727d93ea81c982093f1a2390f98bd1bd0253e379
ixhbinphoenix <47122082+ixhbinphoenix@users.noreply.github.com>
2023-02-25 02:03:36 +0100

    Fix flameshot-wrapper and add directory help to set_wallpaper

--- scripts/set_wallpaper.sh
+++ scripts/set_wallpaper.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-swaymsg "output \"*\" bg $1 fill"
+
+if [[ -f "$1" ]]; then
+  swaymsg "output \"*\" bg $1 fill"
+elif [[ -f "$HOME/Pictures/wallpapers/$1" ]]; then
+  swaymsg "output \"*\" bg $HOME/Pictures/wallpapers/$1 fill"
+else
+  if [[ -f "$(which lsd)" ]]; then
+    lsd -la $HOME/Pictures/wallpapers/
+  else
+    ls -la --color=auto $HOME/Pictures/wallpapers/
+  fi
+fi
